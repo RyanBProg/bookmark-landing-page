b26de8453f896a431094cd8fe38953486a09eb6e
Ryan <ryanbprog@gmail.com>
2024-06-23 17:41:01 +0100

    default rubik font added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        softBlue: "hsl(231, 69%, 60%)",
+        softRed: "hsl(0, 94%, 66%)",
+        grayishBlue: "hsl(229, 8%, 60%)",
+        veryDarkBlue: "hsl(229, 31%, 21%)",
+      },
+      fontFamily: {
+        sans: ["Rubik", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
